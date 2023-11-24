b4083a440d819e59b280f83e57b84558eee6200b
paulinCiany <paulinlubamba24@gmail.com>
2023-11-24 21:26:20 +0200

    mobile v2 and contacts container

---
+++ main.js
@@ -0,0 +1,15 @@
+const prenom = document.querySelector("prenom")
+const nom = document.querySelector("nom")
+const phone = document.querySelector("phone")
+const email =document.querySelector("email")
+const bio  = document.querySelector("bio")
+const btnCreat = document.querySelector("btnCreat")
+const btnRenit = document.querySelector("btnRenit")
+
+
+
+
+function addContatact() {
+    
+    
+}

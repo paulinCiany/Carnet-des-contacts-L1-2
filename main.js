4a8e666b0ae000479c2942e6a452e796685fedcc
paulinCiany <paulinlubamba24@gmail.com>
2023-12-12 15:50:53 +0200

    fixed conflits

--- main.js
+++ main.js
@@ -7,11 +7,13 @@ let phoneNum = document.querySelector("#phone")
 let group = document.querySelector("#group")
 let email = document.querySelector("#Email")
 let bio = document.querySelector(".inputBio")
+let file = document.querySelector("#file")
 let btncreat = document.querySelector(".btnCreat")
+let btnRenit = document.querySelector(".btnRenit")
+let inputDrop = document.querySelector(".inputDrop")
+let pic
+let tabPictureForm = []
 let listContact = []
-
-
-
 
 let telephone = document.getElementById('phone');
 
@@ -54,7 +56,6 @@ email.addEventListener('blur', function () {
 });
 
 // Validate other input fields (name, group, bio)
-// let nameInput = document.getElementById('nom');
 let groupInput = document.getElementById('group');
 let bioInput = document.getElementById('bio');
 
@@ -113,7 +114,7 @@ const prenomError = document.getElementById('prenomError');
 username.addEventListener('blur', function () {
   // Condition 1: Le prénom doit commencer par une lettre
   if (!/^[a-zA-Z]/.test(username.value)) {
-  username.style.borderColor = 'red';
+    username.style.borderColor = 'red';
     prenomError.textContent = 'Le prénom doit commencer par une lettre';
     return; // Arrête l'exécution de la fonction si la condition n'est pas remplie
   }
@@ -126,7 +127,7 @@ username.addEventListener('blur', function () {
   }
 
   // Condition 3: Le prénom ne doit pas contenir de chiffres
-  if (/[0-9]/.test(prenomInput.value)) {
+  if (/[0-9]/.test(username.value)) {
     username.style.borderColor = 'red';
     prenomError.textContent = 'Le prénom ne doit pas contenir de chiffres';
     return;
@@ -137,118 +138,178 @@ username.addEventListener('blur', function () {
   prenomError.textContent = '';
 });
 
-// fin de condtion
+// add picture to the form
+file.addEventListener("change", function () {
 
+  if (file.files[0].size < 1000000) {  // 1MB = 1000000
+    const reader = new FileReader()
+    reader.addEventListener("load", () => { // <img id="photoForApp" class="postionPhoto" width="70px" height="70px">
+      let imgEl = document.createElement("img")
+      imgEl.setAttribute('id', 'photoForApp')
+      imgEl.className = 'postionPhoto'
+      inputDrop.append(imgEl)
+      document.querySelector("#displayNon").remove();
+      document.querySelector("#photoForApp").src = reader.result
+
+      tabPictureForm.pop()
+      tabPictureForm.push(reader.result)
+    })
+    reader.readAsDataURL(this.files[0]);
+  }
+  else {
+    alert("This file is too large!")
+  }
+})
 // fonctions for adding contacts and buttons
 
 btncreat.addEventListener("click", function (e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    listContact.push({
-        prenom: userfirstname.value,
-        nom: username.value,
-        phone: phoneNum.value,
-        groupe: group.value,
-        email: email.value,
-        bio: bio.value,
+  listContact.push({
+    // picture: imgInput.src == undefined ? "./image/Profile Icon.webp" : imgInput.src,
+    picture: tabPictureForm[0],
+    prenom: userfirstname.value,
+    nom: username.value,
+    phone: phoneNum.value,
+    groupe: group.value,
+    email: email.value,
+    bio: bio.value,
+  });
 
-
-    });
-    showContacts();
-    contactField();
+  showContacts();
+  contactField();
 
 })
 
-
 function showContacts() {
-    div.innerHTML = ''
-    for (let i = 0; i < listContact.length; i++) {
-        // const element = listContact[i];
-        // let index = listContact.indexOf(element);
-        console.log(i);
-        let contactdiv = document.createElement("div");
-        contactdiv.classList = "contactIdentity";
-        div.appendChild(contactdiv);
+  div.innerHTML = ''
 
-        let img = document.createElement("div");
-        img.classList = "img_identity";
-        img.scr = "image/home-02-480x480.jpg";
-        img.alt = "ma photo";
-        contactdiv.appendChild(img);
+  console.log(listContact);
 
-        let divIdentity = document.createElement("div");
-        divIdentity.classList = "infoContact";
-        contactdiv.appendChild(divIdentity);
+  for (let i = 0; i < listContact.length; i++) {
+    // const element = listContact[i];
+    // let index = listContact.indexOf(element);
+    console.log(i);
+    let contactdiv = document.createElement("div");
+    contactdiv.classList = "contactIdentity";
+    div.appendChild(contactdiv);
 
-        let names = document.createElement("div");
-        names.classList = "fullNameAndbtns";
-        divIdentity.appendChild(names);
+    let imageDiv = document.createElement('div')
+    imageDiv.className = 'imgDiv';
+    contactdiv.appendChild(imageDiv)
 
-        let personalNames = document.createElement("div");
-        personalNames.classList = "fullname";
-        personalNames.innerHTML = userfirstname.value + " " + username.value + "-" + group.value;
-        names.appendChild(personalNames);
+    let img = document.createElement("img");
+    img.className = 'img_identity'
+    imageDiv.appendChild(img)
+    // img.src = tabPictureForm[0]
+    img.src = listContact[i].picture
 
-        let btndeleteandmodify = document.createElement('div');
-        btndeleteandmodify.classList = "btns";
-        names.appendChild(btndeleteandmodify);
+    let divIdentity = document.createElement("div");
+    divIdentity.classList = "infoContact";
+    contactdiv.appendChild(divIdentity);
 
-        let modifyButton = document.createElement("button");
+    let names = document.createElement("div");
+    names.classList = "fullNameAndbtns";
+    divIdentity.appendChild(names);
 
-        btndeleteandmodify.appendChild(modifyButton);
+    let personalNames = document.createElement("div");
+    personalNames.classList = "fullname";
+    // personalNames.innerHTML = userfirstname.value + " " + username.value + "-" + group.value;
+    personalNames.innerHTML = listContact[i].prenom + " " + listContact[i].nom + "-" + listContact[i].groupe;
+    names.appendChild(personalNames);
 
-        let modifyImg = document.createElement("img")
-        modifyImg.src = "image/Vector.svg";
-        modifyButton.appendChild(modifyImg);
+    let btndeleteandmodify = document.createElement('div');
+    btndeleteandmodify.classList = "btns";
+    names.appendChild(btndeleteandmodify);
 
-        let deleteButton = document.createElement("button");
+    let modifyButton = document.createElement("button");
 
+    btndeleteandmodify.appendChild(modifyButton);
 
-        btndeleteandmodify.appendChild(deleteButton);
+    let modifyImg = document.createElement("img")
+    modifyImg.src = "image/Vector.svg";
+    modifyButton.appendChild(modifyImg);
+    modifyButton.addEventListener("click", function () {
+      editInfo(i)
+    });
 
-        let deleteImg = document.createElement("img");
-        deleteImg.src = "image/Vector (1).svg";
-        deleteButton.addEventListener("click", function () {
-            deleteContact(i)
-        });
-        deleteButton.appendChild(deleteImg);
+    let deleteButton = document.createElement("button");
 
-        let adress = document.createElement("div");
-        adress.classList = "telNum"
-        divIdentity.appendChild(adress);
+    btndeleteandmodify.appendChild(deleteButton);
 
-        let phone = document.createElement("p");
-        phone.innerHTML = phoneNum.value;
-        adress.appendChild(phone);
+    let deleteImg = document.createElement("img");
+    deleteImg.src = "image/Vector (1).svg";
+    deleteButton.addEventListener("click", function () {
+      deleteContact(i)
+    });
+    deleteButton.appendChild(deleteImg);
 
-        let emailP = document.createElement("p");
-        emailP.innerHTML = email.value;
-        adress.appendChild(emailP);
+    let adress = document.createElement("div");
+    adress.classList = "telNum"
+    divIdentity.appendChild(adress);
 
-        let about = document.createElement("div");
-        about.classList = "bioInfo";
-        divIdentity.appendChild(about);
+    let phone = document.createElement("p");
+    phone.innerHTML = listContact[i].phone;
+    adress.appendChild(phone);
 
-        let bioinfoP = document.createElement("p");
-        bioinfoP.innerHTML = bio.value;
-        about.appendChild(bioinfoP);
-    }
+    let emailP = document.createElement("p");
+    emailP.innerHTML = listContact[i].email;
+    adress.appendChild(emailP);
+
+    let about = document.createElement("div");
+    about.classList = "bioInfo";
+    divIdentity.appendChild(about);
+
+    let bioinfoP = document.createElement("p");
+    bioinfoP.innerHTML = listContact[i].bio
+    // bioinfoP.innerHTML = bio.value;
+    about.appendChild(bioinfoP);
+  }
 }
 
 function contactField() {
-    userfirstname.value = "";
-    username.value = "";
-    phoneNum.value = "";
-    group.value = "";
-    email.value = "";
-    bio.value = "";
+  userfirstname.value = "";
+  username.value = "";
+  phoneNum.value = "";
+  group.value = "";
+  email.value = "";
+  bio.value = "";
+  file.value = "";
+  document.querySelector("#photoForApp").remove();
+
+  let spanPhotoApp = document.createElement('span');
+  spanPhotoApp.className = 'spanInfo';
+  spanPhotoApp.setAttribute('id', 'displayNon');
+  inputDrop.appendChild(spanPhotoApp)
+  spanPhotoApp.innerHTML = 'deposez votre photo ici'
 }
+
 function deleteContact(i) {
-    console.log('delete called');
-    listContact.splice(i, 1);
-    console.log(listContact);
-    showContacts();
+  console.log('delete called');
+  listContact.splice(i, 1);
+  console.log(listContact);
+  showContacts();
 }
+// renitializing the form
+btnRenit.addEventListener('click', contactField)
+
+// edit infolet inputDrop = document.querySelector(".inputDrop")
+
+function editInfo(i) {
+  tabPictureForm[0] = listContact[i].picture
+  userfirstname.value = listContact[i].prenom
+  username.value = listContact[i].nom
+  phoneNum.value =  listContact[i].phone
+  group.value =  listContact[i].groupe
+  email.value =  listContact[i].email
+  bio.value =   listContact[i].bio
+
+  btncreat.innerText = "Update"
+  btnRenit.innerText = "Annuler"
+}
+
+
+
 
 
 

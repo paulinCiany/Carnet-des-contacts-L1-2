17cc091a3b3675781056f01a7f0a84391fd73b74
Bibiche96 <bibichekweshibk@gmail.com>
2023-12-15 15:51:45 +0200

    add functions

--- main.js
+++ main.js
@@ -14,134 +14,165 @@ let inputDrop = document.querySelector(".inputDrop")
 let pic
 let tabPictureForm = []
 let listContact = []
-
-let telephone = document.getElementById('phone');
-
-phone.addEventListener('blur', function () {
-  let numerophone = this.value;
-  if (!validatePhone(numerophone)) {
-    this.style.borderColor = 'red';
-    phoneError.textContent = 'Veuillez renseigner un numero correct.';
-  } else if (phone.value.length < 10 || phone.value.length > 10) {
-    phone.style.borderColor = 'red';
-    phoneError.textContent = 'Veuillez renseigner un numero de telephone qui possede 10 chiffre.';
-  } else {
-    phone.style.borderColor = '';
-    phoneError.textContent = '';
-  }
+const prenomError = document.getElementById('prenomError');
+userfirstname.addEventListener('blur', function () {
+  validatePrenom();
 });
-
-// Function to validate phone number format
-function validatePhone(phone) {
-  let format = /^(084|085|080|089|081|082|099|097|090)/;
-  return format.test(phone);
+function validePrenom(re) {
+  re = /^[a-zA-Z]/;
+  return re.test(userfirstname);
 }
+function validatePrenom() {
+  if (validePrenom(userfirstname.value)) {
+    userfirstname.style.borderColor = 'red';
+    prenomError.textContent = 'Le prénom doit commencer par une lettre';
+    return false;
+  }
+  else if (userfirstname.value.length < 3) {
+    userfirstname.style.borderColor = 'red';
+    prenomError.textContent = 'Le prénom doit avoir au moins 3 caractères';
+    return false;
+  } else if (userfirstname.value.length > 50) {
+    userfirstname.style.borderColor = 'red';
+    prenomError.textContent = 'Le prénom ne doit pas avoir plus de 50 caractères';
+    return false;
+  }
+  else if (/[0-9]/.test(userfirstname.value)) {
+    userfirstname.style.borderColor = 'red';
+    prenomError.textContent = 'Le prénom ne doit pas contenir des chiffres';
+    return false;
+  } else {
+    userfirstname.style.borderColor = "";
+    prenomError.textContent = "";
+    return true;
+  }
+}
+const nameError = document.getElementById('nameError');
+username.addEventListener('blur', function () {
+  validateName();
+});
+function valideName(re) {
+  re = /^[a-zA-Z]/;
+  return re.test(username);
+}
+function validateName() {
+  if (valideName(username.value)) {
+    username.style.borderColor = 'red';
+    nameError.textContent = 'Le nom doit commencer par une lettre';
+    return false;
+  }
+  else if (username.value.length < 3) {
+    username.style.borderColor = 'red';
+    nameError.textContent = 'Le nom doit avoir au moins 3 caractères';
+    return false;
+  } else if (username.value.length > 50) {
+    username.style.borderColor = 'red';
+    nameError.textContent = 'Le nom ne doit pas avoir plus de 50 caractères';
+    return false;
+  } else if (/[0-9]/.test(username)) {
+    username.style.borderColor = 'red';
+    nameError.textContent = 'Le nom ne doit contenir que des lettres';
+    return false;
+  }
+  else {
+    username.style.borderColor = '';
+    nameError.textContent = '';
+    return true;
+  }
+}
+const phoneError = document.getElementById('phoneError');
+phoneNum.addEventListener('blur', function () {
+  validatePhone();
+});
+function validatePhone() {
+  let NumFormat = /^(084|085|080|089|081|082|099|097|090)\d{0,7}$/;
+  let RegexNum = NumFormat.test(phoneNum.value);
 
-// Function to validate email format
+  if (!RegexNum) {
+    phoneNum.style.borderColor = 'red';
+    phoneError.textContent = 'Veuillez renseigner un numero correct.';
+
+  } else if (phoneNum.value.length < 10 || phoneNum.value.length > 10) {
+    phoneNum.style.borderColor = 'red';
+    phoneError.textContent = 'Veuillez renseigner un numero de telephone qui possede 10 chiffres.';
+
+  } else {
+    phoneNum.style.borderColor = '';
+    phoneError.textContent = '';
+
+  }
+  if (existNum) {
+    phoneNum.style.borderColor = 'red';
+    phoneError.textContent = 'ce numero exite déjà';
+
+  }
+  else {
+    phoneNum.style.borderColor = '';
+    phoneError.textContent = '';
+
+  }
+}
+// function validetePhone(phone) {
+//   let format = /^(084|085|080|089|081|082|099|097|090)\d{0,7}$/g;
+//   return format.test(phone);
+// }
 function validateEmail(email) {
   let re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return re.test(email);
 }
-// let emailInput = document.getElementById('email');
-let errorMessage = document.getElementById('error');
-
+let emailError = document.getElementById('emailError');
 email.addEventListener('blur', function () {
-  if (!validateEmail(this.value)) {
-    this.style.borderColor = 'red';
+  EmailValidation();
+});
+function EmailValidation() {
+  if (!validateEmail(email)) {
+    email.style.borderColor = 'red';
     emailError.textContent = 'Veuillez renseigner une adresse email valide';
-  } else {
-    this.style.borderColor = '';
+    return false;
+  } else if (existEmail) {
+    email.style.borderColor = 'red';
+    emailError.textContent = 'cet email existe déjà';
+    
+  }
+  else {
+    email.style.borderColor = '';
     emailError.textContent = '';
+   
   }
-});
-
-// Validate other input fields (name, group, bio)
-let groupInput = document.getElementById('group');
-let bioInput = document.getElementById('bio');
-
-const prenomError = document.getElementById('prenomError');
-userfirstname.addEventListener('blur', function () {
-  // Condition 1: Le prénom doit commencer par une lettre
-  if (!/^[a-zA-Z]/.test(userfirstname.value)) {
-    userfirstname.style.borderColor = 'red';
-    prenomError.textContent = 'Le prénom doit commencer par une lettre';
-    return;
-  }
-
-  // Condition 2: Le prénom doit avoir au moins 3 caractères
-  if (userfirstname.value.length < 3) {
-    userfirstname.style.borderColor = 'red';
-    prenomError.textContent = 'Le prénom doit avoir au moins 3 caractères';
-    return;
-  }
-
-  // Condition 3: Le prénom ne doit pas contenir de chiffres
-  if (/[0-9]/.test(userfirstname.value)) {
-    userfirstname.style.borderColor = 'red';
-    prenomError.textContent = 'Le prénom ne doit pas contenir de chiffres';
-    return;
-  }
-
-  // Si toutes les conditions sont remplies, le prénom est valide
-  userfirstname.style.borderColor = '';
-  prenomError.textContent = '';
-});
-
-const groupError = document.getElementById('groupError')
-groupInput.addEventListener('blur', function () {
-  if (groupInput.value.length < 3) {
-    groupInput.style.borderColor = 'red';
-    groupError.textContent = 'Veuillez renseigner un groupe valide.';
+}
+const groupError = document.getElementById('groupError');
+function validateGroupe() {
+  if (group.value == "") {
+    group.style.borderColor = 'red';
+    groupError.textContent = "ce champ ne peut pas etre Vide.";
+    return false;
   } else {
-    groupInput.style.borderColor = '';
-    groupError.textContent = '';
+    group.style.borderColor = "";
+    groupError.textContent = "";
+    return true;
   }
+}
+group.addEventListener('blur', function () {
+  validateGroupe();
 });
-const bioError = document.getElementById('bioError')
-bioInput.addEventListener('blur', function () {
-  if (bioInput.value.length < 10) {
-    bioInput.style.borderColor = 'red';
-    bioError.textContent = 'Veuillez renseigner une bio d\'au moins 10 caractères.';
+const bioError = document.getElementById('bioError');
+function validateBio() {
+  if (bio.value == "") {
+    bio.style.borderColor = 'red';
+    bioError.textContent = "ce champ ne peut pas etre Vide.";
+    return false;
   } else {
-    bioInput.style.borderColor = '';
-    bioError.textContent = '';
+    bio.style.borderColor = "";
+    bioError.textContent = "";
+    return true;
   }
+}
+bio.addEventListener('blur', function () {
+  validateBio();
 });
-
-// const username = document.getElementById('prenomInput');
-const nameError = document.getElementById('nameError');
-
-username.addEventListener('blur', function () {
-  // Condition 1: Le prénom doit commencer par une lettre
-  if (!/^[a-zA-Z]/.test(username.value)) {
-    username.style.borderColor = 'red';
-    nameError.textContent = 'Le nom doit commencer par une lettre';
-    return; // Arrête l'exécution de la fonction si la condition n'est pas remplie
-  }
-
-  // Condition 2: Le prénom doit avoir au moins 3 caractères
-  if (username.value.length < 3) {
-    username.style.borderColor = 'red';
-    nameError.textContent = 'Le nom doit avoir au moins 3 caractères';
-    return;
-  }
-
-  // Condition 3: Le prénom ne doit pas contenir de chiffres
-  if (/[0-9]/.test(username.value)) {
-    username.style.borderColor = 'red';
-    nameErrortextContent = 'Le nom ne doit pas contenir de chiffres';
-    return;
-  }
-
-  // Si toutes les conditions sont remplies, le prénom est valide
-  username.style.borderColor = '';
-  nameError.textContent = '';
-});
-
-// add picture to the form
 file.addEventListener("change", function () {
 
-  if (file.files[0].size < 1000000) {  // 1MB = 1000000
+  if (file.files[0].size < 1000000) {
     const reader = new FileReader()
     reader.addEventListener("load", () => { // <img id="photoForApp" class="postionPhoto" width="70px" height="70px">
       let imgEl = document.createElement("img")
@@ -150,7 +181,6 @@ file.addEventListener("change", function () {
       inputDrop.append(imgEl)
       document.querySelector("#displayNon").remove();
       document.querySelector("#photoForApp").src = reader.result
-
       tabPictureForm.pop()
       tabPictureForm.push(reader.result)
     })
@@ -160,113 +190,40 @@ file.addEventListener("change", function () {
     alert("This file is too large!")
   }
 })
-// fonctions for adding contacts and buttons
-
 btncreat.addEventListener("click", function (e) {
   e.preventDefault();
-
-  listContact.push({
-    // picture: imgInput.src == undefined ? "./image/Profile Icon.webp" : imgInput.src,
-    picture: tabPictureForm[0],
-    prenom: userfirstname.value,
-    nom: username.value,
-    phone: phoneNum.value,
-    groupe: group.value,
-    email: email.value,
-    bio: bio.value,
-  });
-
-  showContacts();
-  contactField();
+  verifyBeforetoCreat();
 
 })
+btnRenit.addEventListener('click', contactField)
+function editInfo(i) {
+  tabPictureForm[0] = listContact[i].picture
+  userfirstname.value = listContact[i].prenom
+  username.value = listContact[i].nom
+  phoneNum.value = listContact[i].phone
+  group.value = listContact[i].groupe
+  email.value = listContact[i].email
+  bio.value = listContact[i].bio
 
-function showContacts() {
-  div.innerHTML = ''
+  btncreat.innerText = "Modifier"
+  btnRenit.innerText = "Annuler"
+}
+function verifyBeforetoCreat() {
 
-  console.log(listContact);
-
-  for (let i = 0; i < listContact.length; i++) {
-    // const element = listContact[i];
-    // let index = listContact.indexOf(element);
-    console.log(i);
-    let contactdiv = document.createElement("div");
-    contactdiv.classList = "contactIdentity";
-    div.appendChild(contactdiv);
-
-    let imageDiv = document.createElement('div')
-    imageDiv.className = 'imgDiv';
-    contactdiv.appendChild(imageDiv)
-
-    let img = document.createElement("img");
-    img.className = 'img_identity'
-    imageDiv.appendChild(img)
-    // img.src = tabPictureForm[0]
-    img.src = listContact[i].picture
-
-    let divIdentity = document.createElement("div");
-    divIdentity.classList = "infoContact";
-    contactdiv.appendChild(divIdentity);
-
-    let names = document.createElement("div");
-    names.classList = "fullNameAndbtns";
-    divIdentity.appendChild(names);
-
-    let personalNames = document.createElement("div");
-    personalNames.classList = "fullname";
-    // personalNames.innerHTML = userfirstname.value + " " + username.value + "-" + group.value;
-    personalNames.innerHTML = listContact[i].prenom + " " + listContact[i].nom + "-" + listContact[i].groupe;
-    names.appendChild(personalNames);
-
-    let btndeleteandmodify = document.createElement('div');
-    btndeleteandmodify.classList = "btns";
-    names.appendChild(btndeleteandmodify);
-
-    let modifyButton = document.createElement("button");
-
-    btndeleteandmodify.appendChild(modifyButton);
-
-    let modifyImg = document.createElement("img")
-    modifyImg.src = "image/Vector.svg";
-    modifyButton.appendChild(modifyImg);
-    modifyButton.addEventListener("click", function () {
-      editInfo(i)
+  if (validateName() && validatePrenom() && validatePhone() && EmailValidation() && validateGroupe() && validateBio()) {
+    listContact.push({
+      picture: tabPictureForm[0],
+      prenom: userfirstname.value,
+      nom: username.value,
+      phone: phoneNum.value,
+      groupe: group.value,
+      email: email.value,
+      bio: bio.value,
     });
-
-    let deleteButton = document.createElement("button");
-
-    btndeleteandmodify.appendChild(deleteButton);
-
-    let deleteImg = document.createElement("img");
-    deleteImg.src = "image/Vector (1).svg";
-    deleteButton.addEventListener("click", function () {
-      deleteContact(i)
-    });
-    deleteButton.appendChild(deleteImg);
-
-    let adress = document.createElement("div");
-    adress.classList = "telNum"
-    divIdentity.appendChild(adress);
-
-    let phone = document.createElement("p");
-    phone.innerHTML = listContact[i].phone;
-    adress.appendChild(phone);
-
-    let emailP = document.createElement("p");
-    emailP.innerHTML = listContact[i].email;
-    adress.appendChild(emailP);
-
-    let about = document.createElement("div");
-    about.classList = "bioInfo";
-    divIdentity.appendChild(about);
-
-    let bioinfoP = document.createElement("p");
-    bioinfoP.innerHTML = listContact[i].bio
-    // bioinfoP.innerHTML = bio.value;
-    about.appendChild(bioinfoP);
+    showContacts();
+    contactField();
   }
 }
-
 function contactField() {
   userfirstname.value = "";
   username.value = "";
@@ -283,30 +240,104 @@ function contactField() {
   inputDrop.appendChild(spanPhotoApp)
   spanPhotoApp.innerHTML = 'deposez votre photo ici'
 }
-
+function showContacts() {
+  div.innerHTML = ''
+  for (let i = 0; i < listContact.length; i++) {
+    let contactdiv = document.createElement("div");
+    contactdiv.classList = "contactIdentity";
+    div.appendChild(contactdiv);
+    let imageDiv = document.createElement('div')
+    imageDiv.className = 'imgDiv';
+    contactdiv.appendChild(imageDiv)
+    let img = document.createElement("img");
+    img.className = 'img_identity'
+    imageDiv.appendChild(img)
+    img.src = listContact[i].picture
+    let divIdentity = document.createElement("div");
+    divIdentity.classList = "infoContact";
+    contactdiv.appendChild(divIdentity);
+    let names = document.createElement("div");
+    names.classList = "fullNameAndbtns";
+    divIdentity.appendChild(names);
+    let personalNames = document.createElement("div");
+    personalNames.classList = "fullname";
+    // personalNames.innerHTML = userfirstname.value + " " + username.value + "-" + group.value;
+    personalNames.innerHTML = listContact[i].prenom + " " + listContact[i].nom + "-" + listContact[i].groupe;
+    names.appendChild(personalNames);
+    let btndeleteandmodify = document.createElement('div');
+    btndeleteandmodify.classList = "btns";
+    names.appendChild(btndeleteandmodify);
+    let modifyButton = document.createElement("button");
+    btndeleteandmodify.appendChild(modifyButton);
+    let modifyImg = document.createElement("img")
+    modifyImg.src = "image/Vector.svg";
+    modifyButton.appendChild(modifyImg);
+    modifyButton.addEventListener("click", function () {
+      editInfo(i)
+    });
+    let deleteButton = document.createElement("button");
+    btndeleteandmodify.appendChild(deleteButton);
+    let deleteImg = document.createElement("img");
+    deleteImg.src = "image/Vector (1).svg";
+    deleteButton.addEventListener("click", function () {
+      let confirmation = confirm("voulez-vous vraiment supprimer?");
+      if (confirmation == true) {
+        deleteContact(i)
+      } else {
+        return false;
+      }
+    });
+    deleteButton.appendChild(deleteImg);
+    let adress = document.createElement("div");
+    adress.classList = "telNum"
+    divIdentity.appendChild(adress);
+    let phone = document.createElement("p");
+    phone.innerHTML = listContact[i].phone;
+    adress.appendChild(phone);
+    let emailP = document.createElement("p");
+    emailP.innerHTML = listContact[i].email;
+    adress.appendChild(emailP);
+    let about = document.createElement("div");
+    about.classList = "bioInfo";
+    divIdentity.appendChild(about);
+    let bioinfoP = document.createElement("p");
+    bioinfoP.innerHTML = listContact[i].bio
+    // bioinfoP.innerHTML = bio.value;
+    about.appendChild(bioinfoP);
+  }
+}
+function verifyEmailExist() {
+  for (const data of listContact)
+    if (data.email) {
+      return true;
+    }
+}
+// function verifyNumExist() {
+//   for (const num of listContact)
+//     if (num.phone) {
+//       return true;
+//     }
+// }
 function deleteContact(i) {
   console.log('delete called');
   listContact.splice(i, 1);
   console.log(listContact);
   showContacts();
 }
-// renitializing the form
-btnRenit.addEventListener('click', contactField)
 
-// edit infolet inputDrop = document.querySelector(".inputDrop")
+const existNum = listContact.some((element) => element.phoneNum==phoneNum.value)
 
-function editInfo(i) {
-  tabPictureForm[0] = listContact[i].picture
-  userfirstname.value = listContact[i].prenom
-  username.value = listContact[i].nom
-  phoneNum.value =  listContact[i].phone
-  group.value =  listContact[i].groupe
-  email.value =  listContact[i].email
-  bio.value =   listContact[i].bio
 
-  btncreat.innerText = "Modifier"
-  btnRenit.innerText = "Annuler"
-}
+const existEmail = listContact.some((element) => element.email== email.value)
+
+
+
+
+
+
+
+
+
 
 
 
